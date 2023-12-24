650d37d61a31f6c4e344f1f184d5936cd8efadf1
Robert Arcaleanu <alumnerobert@gmail.com>
2023-12-24 16:03:29 +0100

    using .txt instead of PDF as knowledge db

--- llm/create_vectorstore.py
+++ llm/create_vectorstore.py
@@ -18,13 +18,14 @@ def create_vectorstore(api_key, FILE_PATH, DB_PATH):
     if not (os.path.exists(DB_PATH) and os.path.isdir(DB_PATH)):
         print("==========creating vectorstore==========")
         # Load the data
-        loader = PyPDFLoader(FILE_PATH)
-        docs = loader.load_and_split()
+        loader = TextLoader(FILE_PATH)
+        documents = loader.load()
+
+        text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=30, chunk_overlap=0)
+        docs = text_splitter.split_documents(documents)
 
         # Embed the documents
         embedding_function = OpenAIEmbeddings(openai_api_key=api_key)
-        # db = Chroma.from_documents(docs, embedding_function, persist_directory=DB_PATH)
-        # db.persist()
         db = FAISS.from_documents(docs, embedding_function)
         db.save_local(DB_PATH)
         
